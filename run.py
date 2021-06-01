ec48c4c9ac4a42efc76204980f03b58f29b41f1a
Josh Nicholas <hi@thambi.li>
2021-06-01 16:11:04 +1000

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 print("Running scraper...")
 
-import qld_scraper
+# import qld_scraper
 
 print("Making bar chart")
 
-import infect_source_barchart
+# import infect_source_barchart
 
 print("Making line chart")
 
-import locally_acquired_line
+# import locally_acquired_line
 
 print("Map feed")
 
-import qld_map_feed
+# import qld_map_feed
 
 print("Hotspots table")
 
-import qld_hotspot_scraper
+# import qld_hotspot_scraper
